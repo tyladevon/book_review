778b16088d813a43481aa0a2a91d6a861fbac3e9
Tyla Devon <tyladevon@gmail.com>
2020-02-21 11:01:17 -0700

    Refactor NYT API call

--- app/poros/review_service.rb
+++ app/poros/review_service.rb
@@ -4,13 +4,14 @@ class ReviewService
   end
 
   def search 
-    conn.get('/reviews.json') do |req|
+    results = conn.get('svc/books/v3/reviews.json') do |req|
       req.params['title'] = @title
-      req.params['api-key'] =ENV['NYT_API']
-      end 
+      req.params['api-key'] = ENV['NYT_API']
+    end 
+      JSON.parse(results.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new(url: "https://api.nytimes.com/svc/books/v3")
+    Faraday.new(url: "https://api.nytimes.com")
   end 
 end
