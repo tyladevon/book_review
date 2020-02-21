0aa1cce6a6cba5e01880753bc70d75bc93d54af6
Tyla Devon <tyladevon@gmail.com>
2020-02-21 10:18:09 -0700

    Refactor Search Controller for Book Search method call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     search = SearchFacade.new(params[:title])
     render locals: {
-      book: search.book_info,
+      book: search.book_return,
       reviews: search.book_reviews
     }
   end 
